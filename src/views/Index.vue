0a8c00a98d4b8bf682b0528e30292a339febf078
schaoss <ma.gary729@gmail.com>
2018-10-21 00:22:55 +0800

    modify index

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="index">
+  <div id="index">
     <h1> Web Audio </h1>
+    <div class="content">
+      <div>
+        <p>如同一道好料理需要具備色、香、味，一個好網站除了好內容之外，也需要透過互動性更高的視覺、聽覺，來與內容相輔相成。</p>
+        <p>本系列將從 Web Audio API 出發，以說明搭配範例，從實作中逐步認識 API 規格；並挑選數個主流、有趣的 Audio 相關套件，透過 JavaScript，玩轉聲音於程式的字裡行間。</p>
+        <p>歡迎您與我一同踏上這趟旅程，讓我們於科技與藝術的交會處相逢，一起在音樂中肆意漫遊。</p>
+      </div>
+      <img src="https://svgsilh.com/svg/33908-795548.svg" />
+
+      <p>程式碼放在 <a href="https://github.com/schaoss/web-audio">Github</a></p>
+      <p>參賽文章在 <a href="https://ithelp.ithome.com.tw/users/20111380/ironman/1783">這裡</a></p>
+    </div>
   </div>
 </template>
 
@@ -9,3 +20,22 @@ export default {
   name: 'index',
 };
 </script>
+
+<style lang="scss" scoped>
+#index {
+  max-height: 100vh;
+  overflow: auto;
+  .content {
+    max-width: 50vw;
+    margin: 40px auto;
+    > div {
+      text-align: left;
+      line-height: 1.8;
+      white-space: 1;
+    }
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
